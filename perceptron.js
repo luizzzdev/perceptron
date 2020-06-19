8a2213a65db2c5ae4deb88efd6711ab5aa9bc806
Luiz Fernando Rodrigues <luiiz_roodrigues@hotmail.com>
2020-06-18 21:37:01 -0300

    refatoracao para multiplos neuronios

--- perceptron.js
+++ perceptron.js
@@ -1,9 +1,9 @@
-const teta = 0.2
-const alpha = 1
+const teta = 0.2;
+const alpha = 1;
 
-const pesoPadrao = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+const pesoPadrao = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-const copiarPesoPadrao = () => [...pesoPadrao]
+const copiarPesoPadrao = () => [...pesoPadrao];
 
 const pesos = [
   [copiarPesoPadrao(), copiarPesoPadrao(), copiarPesoPadrao()],
@@ -22,256 +22,312 @@ const pesos = [
   [copiarPesoPadrao(), copiarPesoPadrao(), copiarPesoPadrao()],
   [copiarPesoPadrao(), copiarPesoPadrao(), copiarPesoPadrao()],
   [copiarPesoPadrao(), copiarPesoPadrao(), copiarPesoPadrao()],
-]
+];
 
 // 10 16
 
 // console.log(pesos.length)
 
 const zero = [
-  0, 1, 0,
-  1, 0, 1,
-  1, 0, 1,
-  1, 0, 1,
-  0, 1, 0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  1,
+  1,
+  0,
+  1,
+  1,
+  0,
+  1,
+  0,
+  1,
+  0,
   // fixo
-  1
-]
+  1,
+];
 
 const um = [
-  0, 1, 0,
-  1, 1, 0,
-  0, 1, 0,
-  0, 1, 0,
-  1, 1, 1,
+  0,
+  1,
+  0,
+  1,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  1,
+  1,
+  1,
   // fixo
-  1
-]
+  1,
+];
 
 const dois = [
-  0, 1, 0,
-  1, 0, 1,
-  0, 0, 1,
-  0, 1, 0,
-  1, 1, 1,
+  0,
+  1,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const tres = [
-  1, 1, 1,
-  0, 0, 1,
-  1, 1, 1,
-  0, 0, 1,
-  1, 1, 1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const quatro = [
-  1, 0, 1,
-  1, 0, 1,
-  1, 1, 1,
-  0, 0, 1,
-  0, 0, 1,
+  1,
+  0,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const cinco = [
-  1, 1, 1,
-  1, 0, 0,
-  1, 1, 1,
-  0, 0, 1,
-  1, 1, 1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const seis = [
-  1, 1, 1,
-  1, 0, 0,
-  1, 1, 1,
-  1, 0, 1,
-  1, 1, 1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const sete = [
-  1, 1, 1,
-  0, 0, 1,
-  0, 0, 1,
-  0, 0, 1,
-  0, 0, 1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const oito = [
-  1, 1, 1,
-  1, 0, 1,
-  1, 1, 1,
-  1, 0, 1,
-  1, 1, 1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
 const nove = [
-  1, 1, 1,
-  1, 0, 1,
-  1, 1, 1,
-  0, 0, 1,
-  1, 1, 1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
   //fixo
-  1
-]
+  1,
+];
 
-const targets = [
-  [1, 1, 1],
-  [1, 1, -1],
-  [1, -1, 1],
-  [1, -1, -1],
-  [-1, 1, 1],
-  [-1, 1, -1],
-  [-1, -1, -1],
-  [1, 1, 0],
-  [1, -1, 0],
-  [1, 0, 1],
-]
+// const targets = [
+//   [1, 1, 1],
+//   [1, 1, -1],
+//   [1, -1, 1],
+//   [1, -1, -1],
+//   [-1, 1, 1],
+//   [-1, 1, -1],
+//   [-1, -1, -1],
+//   [1, 1, 0],
+//   [1, -1, 0],
+//   [1, 0, 1],
+// ]
 
-const numeros = [
-  zero,
-  um,
-  dois,
-  tres,
-  quatro,
-  cinco,
-  seis,
-  sete,
-  oito,
-  nove
-]
+const neuronios = [
+  {
+    targets: [1, 1, 1, 1, -1, -1, -1, 1, 1, 1],
+    pesos: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    targets: [1, 1, -1, -1, 1, 1, -1, 1, -1, 0],
+    pesos: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    targets: [1, -1, 1, -1, 1, -1, -1, 0, 0, 1],
+    pesos: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+];
 
-const getIndexPeso = (indexNumero, epocas) => {
-  if (indexNumero === 0 && epocas === 1) {
-    return indexNumero
-  }
+const numeros = [zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove];
 
-  if (indexNumero === 0 && epocas > 1) {
-    return numeros.length - 1
-  }
+const calcularSoma = (numeros, pesos) => {
+  let soma = 0;
 
-  return indexNumero - 1
-}
+  numeros.forEach((numero, index) => {
+    soma += numero * pesos[index];
 
-const yent = (numero, indexNumero, epocas) => {
-  const target = targets[indexNumero]
+    // console.log(`Numero ${numero}  Peso ${pesos[index]}`)
+  });
 
-  const indexPeso = getIndexPeso(indexNumero, epocas)
+  return soma;
+};
 
-
-  // console.log(`Numero ${indexNumero} IndexPeso ${indexPeso}`)
-
-  return target.map((valorTarget, indexTarget) => {
-    let result = 0
-
-    numero.forEach((numeroValue, indexValorPeso) => {
-
-      const peso = pesos[indexPeso][indexTarget][indexValorPeso]
-      result += peso * numeroValue
-    })
-
-    return result
-  })
-}
-
-const funcYent = (yents) => {
-  return yents.map(valor => {
-    if(valor > teta) return 1
-    if(valor >= (teta * -1) || valor <= teta) return 0
-    if(valor < (teta * -1)) return -1
-  })
-}
-
-// const resultFuncYent = funcYent(yent(zero))
-// X[10][16] T[10][3]
-const variacaoPeso = () => {
-  return alpha
-}
-
-// yents.lenght === numero de neuronios que nós temos
-// então precisamos atualizar os pesos de cada neuronio
-const atualizarPesosDoNumero = ({ numero, indexNumero, yents, epocas }) => {
-  // [true, false, true]
-  const atualizados = yents.map((valorFYent, indexYent) => {
-    let atualizou = false
-
-    const valorTarget = targets[indexNumero][indexYent]
-
-    numero.forEach((valorNumero, indexValorNumero) => {
-      //  1 * (1 - 0) * valorNumero -> 1 * numero
-      const variacao = alpha * (valorTarget - valorFYent) * valorNumero
-
-      const indexPesoAntigo = getIndexPeso(indexNumero, epocas)
-
-      const pesoAntigo = pesos[indexPesoAntigo][indexYent][indexValorNumero]
-
-      const novoPeso = pesoAntigo + variacao
-
-      pesos[indexNumero][indexYent][indexValorNumero] = pesoAntigo + variacao
-
-      if (pesoAntigo !== novoPeso) atualizou = true
-    })
-
-    return atualizou
-  })
-
-  // true se algum neuronio atualizou
-  return atualizados.some(neuronioAtualizou => !!neuronioAtualizou)
-}
-
-const verificarSaidasComTarget = (primeiroArray, segundoArray) => {
-    console.log(primeiroArray, segundoArray)
-    return primeiroArray.every((elemento, index) => elemento === segundoArray[index])
-}
+const calcularResultadoYent = (valor) => {
+  if (valor > teta) return 1;
+  if (valor >= teta * -1 || valor <= teta) return 0;
+  if (valor < teta * -1) return -1;
+};
 
 const run = () => {
-  let atualizou = true
-  let epocas = 1
+  let atualizou = true;
+  let epocas = 1;
 
-  let linha = 0
+  let linha = 0;
 
   do {
-    linha = 0
-    console.log(`Epoca atual: `, epocas)
-    // index é a linha de entrada da tabela principal
-    numeros.forEach((numero, index) => {
-      atualizou = false
-      // [0, 0, 0,] um para cada neuronio
-      const yents = yent(numero, index, epocas)
+    console.log('Epoca atual: ', epocas);
 
-      const funcYents = funcYent(yents)
+    atualizou = false;
 
-      const saidasSaoIguais = verificarSaidasComTarget(funcYents, targets[index])
+    neuronios.forEach((neuronio) => {
+      numeros.forEach((numero, index) => {
+        const soma = calcularSoma(numero, neuronio.pesos);
 
-      if (!saidasSaoIguais) {
-        atualizou = true
-        atualizarPesosDoNumero({
-          numero,
-          epocas,
-          indexNumero: index,
-          yents: funcYents
-        })
-      }
-    })
+        // console.log(`Soma ${soma}`)
 
-    // remover isso aqui, deixei para garantir
-    // que nao vai ser infinito
-    if (epocas === 100) atualizou = false
-    epocas += 1
-  } while(atualizou === true)
-}
+        const resultadoYent = calcularResultadoYent(soma);
 
-run()
+        const target = neuronio.targets[index];
+
+        // console.log(`Target ${target}`)
+
+        // console.log(`Resultado yent ${resultadoYent} Target ${target}`)
+
+        if (resultadoYent !== target) {
+          atualizou = true;
+          const atualizarPesos = (target, resultadoYent, numero) => {
+            numero.forEach((valorNumero, indexNumero) => {
+              const variacao = alpha * (target - resultadoYent) * valorNumero;
+
+              const pesoAntigo = neuronio.pesos[indexNumero];
+              const pesoNovo = pesoAntigo + variacao;
+
+              if (epocas > 1000000) {
+                console.log(
+                  `Target ${target} Variacao ${variacao} Peso antigo ${pesoAntigo} Peso novo ${pesoNovo}`
+                );
+              }
+
+              neuronio.pesos[indexNumero] = pesoNovo;
+            });
+          };
+
+          atualizarPesos(target, resultadoYent, numero);
+        }
+      });
+    });
+
+    // if (epocas === 10) atualizou = false
+    epocas += 1;
+  } while (atualizou === true);
+};
+
+run();
